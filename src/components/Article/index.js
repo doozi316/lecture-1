a982297f60cf7083f5fc4c49947b495a1b37cd5c
김현진 <hyeonjin.kim@innowireless.com>
2022-10-20 11:31:22 +0900

    bottleneck 현상 해결

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -8,8 +8,7 @@ function zeroPad(value, len) {
 }
 
 /* 파라미터 참고: https://unsplash.com/documentation#supported-parameters */
-function 
-getParametersForUnsplash({ width, height, quality, format }) {
+function getParametersForUnsplash({ width, height, quality, format }) {
     return `?w=${width}&h=${height}&q=${quality}&fm=${format}&fit=crop`;
 }
 
@@ -18,36 +17,41 @@ getParametersForUnsplash({ width, height, quality, format }) {
  * (Markdown으로 된 문자열의 특수문자를 제거하기 위함)
  * */
 function removeSpecialCharacter(str) {
-    const removeCharacters = [
-        '#',
-        '_',
-        '*',
-        '~',
-        '&',
-        ';',
-        '!',
-        '[',
-        ']',
-        '`',
-        '>',
-        '\n',
-        '=',
-        '-',
-    ];
-    let _str = str;
-    let i = 0,
-        j = 0;
+    //     const removeCharacters = [
+    //         '#',
+    //         '_',
+    //         '*',
+    //         '~',
+    //         '&',
+    //         ';',
+    //         '!',
+    //         '[',
+    //         ']',
+    //         '`',
+    //         '>',
+    //         '\n',
+    //         '=',
+    //         '-',
+    //     ];
+    //     let _str = str;
+    //     let i = 0,
+    //         j = 0;
 
-    for (i = 0; i < removeCharacters.length; i++) {
-        j = 0;
-        while (j < _str.length) {
-            if (_str[j] === removeCharacters[i]) {
-                _str = _str.substring(0, j).concat(_str.substring(j + 1));
-                continue;
-            }
-            j++;
-        }
-    }
+    //     for (i = 0; i < removeCharacters.length; i++) {
+    //         j = 0;
+    //         while (j < _str.length) {
+    //             if (_str[j] === removeCharacters[i]) {
+    //                 _str = _str.substring(0, j).concat(_str.substring(j + 1));
+    //                 continue;
+    //             }
+    //             j++;
+    //         }
+    //     }
+
+    //     return _str;
+
+    let _str = str.substring(0, 300);
+    _str.replace(/[#_*~&;![\]`>\n=-]/g, '');
 
     return _str;
 }
